8d5cbdbfdb31ffea896a59d9fd4906e9d0968a78
Vinsonc6 <vinsonc6@nycstudents.net>
2025-10-29 12:17:53 -0400

    e

---
+++ .vscode/app2.js
@@ -0,0 +1,45 @@
+
+const lego = [
+    {
+    name: "Lego Death star",
+     price: 1000.00,
+     inStock: true, 
+     brand : "Legos",
+    }, {
+    name: "Lego Millennium Falcon",
+     price: 649.99, 
+         inStock: true, 
+         brand: "Legos", 
+         images: "https://i5.walmartimages.com/asr/625df3e7-f408-4b69-a52a-4392886a5291.ca5a8df751327ab30c7bb0996141f533.jpeg"
+    }, {
+    name: "Lego R2D2", 
+    price: 230.00,  
+    inStock: true, 
+    brand : "Legos",
+    }, {
+    names: "Lego star wars",
+    price: 250.00, 
+    instock: true,
+    brand: "Legos"
+    }, { 
+    name: "Lego brick",
+    price: 0.03, 
+    instock: true,
+    brand: "Legos"
+    }, { 
+    name: "Lego Star Destoryer", 
+    price: 400.00,
+    instock: true, 
+    brand: "Legos"
+    }, {
+    name: "Lego Super Star Destoryer",
+    price: 1000.00,
+    instock:true,
+    brand: "legos"
+    }, {
+    name: "Lego Star War Imperial Assault Carrier",
+    price: 200.00,
+    instock: true,
+    brand: "legos"
+    }
+]
